f0669bee319dd5efca7d5434d67b6f8bae0405de
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2024-07-08 14:14:37 +0200

    feature: introduce support for `laminas/laminas-cache` v4
    This also introduces `ExtMongoDbResourceManagerInterface` to allow alternative resource manager implementations.
    Since `laminas/laminas-cache` v4 does provided dedicated `Metadata` objects per adapter, the `ext-mongodb` adapter now returns its own `Metadata` object providing the object ID of the document which holds the cache value.
    
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/integration/Psr/CacheItemPool/ExtMongoDbIntegrationTest.php
+++ test/integration/Psr/CacheItemPool/ExtMongoDbIntegrationTest.php
@@ -5,15 +5,23 @@ declare(strict_types=1);
 namespace LaminasTest\Cache\Storage\Adapter\Psr\CacheItemPool;
 
 use Laminas\Cache\Storage\Adapter\ExtMongoDb;
+use Laminas\Cache\Storage\Adapter\ExtMongoDbOptions;
+use Laminas\Cache\Storage\FlushableInterface;
 use Laminas\Cache\Storage\Plugin\Serializer;
-use Laminas\Cache\Storage\PluginAwareInterface;
 use Laminas\Cache\Storage\StorageInterface;
+use Laminas\Serializer\AdapterPluginManager;
+use Laminas\ServiceManager\ServiceManager;
 use LaminasTest\Cache\Storage\Adapter\AbstractCacheItemPoolIntegrationTest;
 
 use function getenv;
 use function sprintf;
 
-class ExtMongoDbIntegrationTest extends AbstractCacheItemPoolIntegrationTest
+/**
+ * @uses FlushableInterface
+ *
+ * @template-extends AbstractCacheItemPoolIntegrationTest<ExtMongoDbOptions>
+ */
+final class ExtMongoDbIntegrationTest extends AbstractCacheItemPoolIntegrationTest
 {
     private const LONG_KEY_SUPPORT_POSTPONED = 'Long key support will be provided with a dedicated ticket.';
 
@@ -29,7 +37,7 @@ class ExtMongoDbIntegrationTest extends AbstractCacheItemPoolIntegrationTest
         $this->skippedTests['testBasicUsageWithLongKey']                        = self::LONG_KEY_SUPPORT_POSTPONED;
     }
 
-    protected function createStorage(): StorageInterface
+    protected function createStorage(): StorageInterface&FlushableInterface
     {
         $storage = new ExtMongoDb([
             'server'     => (string) getenv('TESTS_LAMINAS_CACHE_EXTMONGODB_CONNECTSTRING'),
@@ -37,8 +45,7 @@ class ExtMongoDbIntegrationTest extends AbstractCacheItemPoolIntegrationTest
             'collection' => (string) getenv('TESTS_LAMINAS_CACHE_EXTMONGODB_COLLECTION'),
         ]);
 
-        self::assertInstanceOf(PluginAwareInterface::class, $storage);
-        $storage->addPlugin(new Serializer());
+        $storage->addPlugin(new Serializer(new AdapterPluginManager(new ServiceManager())));
         return $storage;
     }
 }
